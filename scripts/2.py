78018d83eb0624e99d8dd33cf263d44c49577d1b
gael-lycee <gael.muller.lycee@gmail.com>
2025-10-04 15:50:59 +0200

    Create 2.py

---
+++ scripts/2.py
@@ -0,0 +1,16 @@
+#Boucle si
+
+a = int("entre un nombre entier")
+
+if a == 1:
+    print("a est égale a 1")
+
+elif (a != 4) or (a/2==4):
+    print("a n'est pas égale a 4")
+    print("ou")
+    print("a diviser par 2 est égale a 4")
+
+else:
+    print("a n'est pas égale a 1")
+    print("a est égale a 4")
+    print("a diciser pas 2 n'est pas égale a 4")
