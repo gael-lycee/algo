3e1de3f4552a84970780e2a87e0149dc7aa1fd7c
gael-lycee <gael.muller.lycee@gmail.com>
2025-10-04 14:18:29 +0200

    Create 1.py

---
+++ scripts/1.py
@@ -0,0 +1,23 @@
+from math import *
+
+a = 1
+
+b = "Hello Word"
+
+c = 3.14
+
+d = eval(input("entre ce que tu veux : "))
+
+print(f"a = {a}, b = {b} , c = {c} , d = {d}")
+
+
+
+a,b,c,d=5,5,5,5
+
+a = a*3
+
+b = b**2
+
+c = c/2
+
+d = d+3
