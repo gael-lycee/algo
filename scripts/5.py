7f4f87138b89a2f6d39ee3369343af4447337d14
gael-lycee <gael.muller.lycee@gmail.com>
2025-10-06 22:33:10 +0200

    Update 5.py

--- scripts/5.py
+++ scripts/5.py
@@ -1,1 +1,15 @@
+#boucle while avec variable et condition d'arrêt sur le solde d'un compte bancaire        scritpe 1
 
+solde = 100
+
+
+retrait = eval(input("Montant du retrait en euros : "))
+
+while solde >= retrait:
+    print("Retrait de", retrait, "euros.")
+
+    solde = solde - retrait
+
+    print("Solde restant :", solde, "euros.")
+
+print("Solde insuffisant pour un nouveau retrait.")
